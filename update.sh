256a4da71df8b1a89408ce110edb803629666c74
hobroker <igor.leahu24@gmail.com>
2019-11-30 21:37:35 +0200

    change release.yml workflow

--- update.sh
+++ update.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-git stash
-git pull origin HEAD
-npm install
-npm run build
-npm link
+url="https://github.com/hobroker/altum/releases/download/latest/altum-latest.tar.gz"
+
+curl url --output altum-latest.tar.gz
+npm install altum-latest.tar.gz
